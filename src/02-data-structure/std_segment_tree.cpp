713384cab227bb6fc78ad5d16cb179d75996de38
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2022-03-01 09:53:20 -0500

    feat: std segment tree was added

---
+++ src/02-data-structure/std_segment_tree.cpp
@@ -0,0 +1,35 @@
+using int64 = long long;
+const int64 nil = 1e18; // for sum: 0, for min: 1e18, for max: -1e18
+int64 op(int64 x, int64 y) { return min(x, y); }
+struct segtree {
+    segtree *left, *right;
+    int l, r, m;
+    int64 sum;
+    segtree(int l, int r) : l(l), r(r), sum(nil) {
+        if(l != r) {
+            m = (l+r)/2;
+            left = new segtree(l, m);
+            right = new segtree(m+1, r);
+        }
+    }
+    void modify(int pos, int v) {
+        if(l == r) {
+            sum = v;
+        } else {
+            if(pos <= m) left->modify(pos, v);
+            else right->modify(pos, v);
+            sum = op(left->sum, right->sum);
+        }
+    }
+    int64 query(int a, int b) {
+        if(a > r || b < l) return nil;
+        if(a <= l && r <= b) return sum;
+        return op(left->query(a, b), right->query(a, b));
+    }
+};
+// Usage:
+// segtree st(0, n);
+// forn(i, n) {
+//    cin >> val;
+//    st.modify(i, val);
+// }
