2a1a16642fbfb4335e9685cb7d8f30d5f09847b0
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2022-03-02 15:45:57 -0500

    feat: prefix function was updated and minimum expresion was added

---
+++ src/04-string/minimum-expression.cpp
@@ -0,0 +1,12 @@
+// O(n)
+int minimum_expression(string s) {
+    s = s+s; // si no se concatena devuelve el indice del sufijo menor
+    int len = s.size(), i = 0, j = 1, k = 0;
+    while (i+k < len && j+k < len) {
+        if (s[i+k] == s[j+k]) k++;
+        else if (s[i+k] > s[j+k]) i = i+k+1, k = 0; // cambiar por < para maximum
+        else j = j+k+1, k = 0;
+        if (i == j) j++;
+    }
+    return min(i, j);
+}
