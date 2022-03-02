f73eeeb61215f33030bdb0a3e3a861ab3b4bd7db
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2022-03-01 21:40:56 -0500

    feat: std library was renamed

--- src/00-miscellaneous/misc.cpp
+++ src/00-miscellaneous/misc.cpp
@@ -43,3 +43,21 @@ inline void unique(vector<T> &v) {
 //   * 62x10^6 Elements of 32 Bit (4 bytes) is equal to 250 MB
 //   * 10^6 Elements of 64 Bits (8 bytes) is equal to 8 MB
 //   * 31x10^6 Elements of 64 Bit (8 bytes) is equal to 250 MB
+
+ios::sync_with_stdio(0);
+cin.tie(0);
+
+// Lectura segun el tipo de dato (Se usan las mismas para imprimir):
+
+scanf("%d", &value); //int
+scanf("%ld", &value); //long y long int
+scanf("%c", &value); //char 
+scanf("%f", &value); //float
+scanf("%lf", &value); //double
+scanf("%s", &value); //char*
+scanf("%lld", &value); //long long int
+scanf("%x", &value); //int hexadecimal
+scanf("%o", &value); //int octal
+
+// Impresion de punto flotante con d decimales, ejemplo 6 decimales:
+printf("%.6lf", value);
