090aba0190cd7089e5f4b1bfaf4fd67bc552c034
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2022-03-01 20:41:03 -0500

    feat: z_algorithm and aho corasick algorithm were added

---
+++ src/04-string/aho_corasick.cpp
@@ -0,0 +1,61 @@
+// El trie (o prefix tree) guarda un diccionario de strings como un arbol enraizado.
+// Aho corasick permite encontrar las ocurrencias de todos los strings del trie en un string s.
+
+const int alpha = 26; // cantidad de letras del lenguaje
+const char L = 'a'; // primera letra del lenguaje
+
+struct node {
+    int next[alpha], end;
+    int link, exit, cnt;
+    int& operator[](int i) { return next[i]; }
+};
+
+vector<node> trie = {node()};
+
+void add_str(string &s, int id = 1) {
+    int u = 0;
+    for (auto ch : s) {
+        int c = ch-L;
+        if (!trie[u][c]) {
+            trie[u][c] = trie.size();
+            trie.push_back(node());
+        }
+        u = trie[u][c];
+    }
+    trie[u].end = id; //con id > 0
+    trie[u].cnt++;
+}
+
+// aho corasick
+void build_ac() {
+    queue<int> q; q.push(0);
+    while (q.size()) {
+        int u = q.front(); q.pop();
+        for (int c = 0; c < alpha; ++c) {
+            int v = trie[u][c];
+            if (!v) trie[u][c] = trie[trie[u].link][c];
+            else q.push(v);
+            if (!u || !v) continue;
+            trie[v].link = trie[trie[u].link][c];
+			trie[v].exit = trie[trie[v].link].end ? trie[v].link : trie[trie[v].link].exit;
+            trie[v].cnt += trie[trie[v].link].cnt;
+        }
+    }
+}
+
+vector<int> cnt; //cantidad de ocurrencias en s para cada patron
+
+void run_ac(string &s) {
+    int u = 0, sz = s.size();
+    for (int i = 0; i < sz; ++i) {
+        int c = s[i]-L;
+        while (u && !trie[u][c]) u = trie[u].link;
+        u = trie[u][c];
+        int x = u;
+        while (x) {
+            int id = trie[x].end;
+            if (id) cnt[id-1]++;
+            x = trie[x].exit;
+        }
+    }
+}
