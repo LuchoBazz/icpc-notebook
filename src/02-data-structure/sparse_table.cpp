e1e4dd9c22b677e8c2d3d73af17f55c80e9c798a
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2022-08-16 08:40:17 -0500

    feat: RMQ Sparce Table was updated

--- src/02-data-structure/sparse_table.cpp
+++ src/02-data-structure/sparse_table.cpp
@@ -1,25 +1,15 @@
-template<typename T>
-class SparseTable {
-public:
-    int n;
-    vector<vector<T>> table;
-
-    SparseTable(const vector<T>& v) {
-        n = (int) v.size();
-        int max_log = 32 - __builtin_clz(n);
-        table.resize(max_log);
-        table[0] = v;
-        for (int j = 1; j < max_log; j++) {
-            table[j].resize(n - (1 << j) + 1);
-            for (int i = 0; i <= n - (1 << j); i++) {
-                table[j][i] = min(table[j - 1][i], table[j - 1][i + (1 << (j - 1))]);
-            }
-        }
+struct RMQ {
+    vector<vector<int>> table;
+    RMQ(vector<int> &v) : table(20, vector<int>(v.size())) {
+        int n = v.size();
+        for (int i = 0; i < n; i++)
+            table[0][i] = v[i];
+        for (int j = 1; (1<<j) <= n; j++)
+            for (int i = 0; i + (1<<(j-1)) < n; i++)
+                table[j][i] = min(table[j-1][i], table[j-1][i + (1<<(j-1))]);
     }
-
-    T query(int from, int to) const {
-        assert(0 <= from && from <= to && to <= n - 1);
-        int lg = 32 - __builtin_clz(to - from + 1) - 1;
-        return min(table[lg][from], table[lg][to - (1 << lg) + 1]);
+    int query(int a, int b) {
+        int j = 31 - __builtin_clz(b-a+1);
+        return min(table[j][a], table[j][b-(1<<j)+1]);
     }
 };
